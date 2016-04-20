f3dae832bb8406cd0dd61ff14a6f61fee797a15b
Kim minsung <leynuxms@gmail.com>
2016-04-20 21:29:47 +0900

    숙제 답안

---
+++ 연구자료/숙제 1일차 답안.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+
+a = 10
+print "숫자 맞추기 게임입니다."
+while True:
+
+    g = input("숫자를 입력하세요 : ")
+    if a > g:
+        print "숫자가 작습니다.다른 숫자를 입력해 보세요"
+    elif a == g:
+        print "맞췄습니다! 게임을 끝내겠습니다."
+        break
+    else:
+        print "숫자가 큽니다. 다른 숫자를 입력해 보세요"
